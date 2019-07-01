9cdb57c5ac54feb59568f2ea92ca2ca22af8bbee
Cameron Auser <camauser@gmail.com>
2019-07-01 16:28:42 -0600

    Cleaning up code for creating the UI.

--- src/main/edus2/application/EDUS2View.java
+++ src/main/edus2/application/EDUS2View.java
@@ -23,6 +23,7 @@ import edus2.domain.Scan;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
@@ -35,8 +36,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -130,8 +129,66 @@ public class EDUS2View extends Application
             LoggerSingleton.logErrorIfEnabled("Failed to load scans from " + EDUS2_SAVE_FILE_NAME + ". Reason: " + e.getMessage());
         }
 
-        // Setting up the actual interface of the program
         main = new BorderPane();
+        main.setTop(generateTop());
+        main.setBottom(generateBottom(stage));
+
+        Scene scene = new Scene(main);
+        stage.setHeight(720);
+        stage.setWidth(1280);
+        stage.setScene(scene);
+        stage.getIcons().add(getThumbnailImage());
+        stage.setTitle("EDUS2J");
+        stage.show();
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                processScanRequest();
+            } else {
+                currentScan += event.getText();
+            }
+        });
+    }
+
+    private Node generateBottom(Stage stage) {
+        Button btnAbout = new Button("About");
+        btnAbout.setFont(BUTTON_FONT);
+        Button btnFullscreen = new Button("Toggle Fullscreen");
+        btnFullscreen.setFont(BUTTON_FONT);
+        Button btnSettings = new Button("Settings");
+        btnSettings.setFont(BUTTON_FONT);
+        Button btnQuit = new Button("Quit");
+        btnQuit.setFont(BUTTON_FONT);
+
+        HBox buttons = new HBox();
+        buttons.getChildren().addAll(btnAbout, btnFullscreen, btnSettings,
+                btnQuit);
+        buttons.setAlignment(Pos.BOTTOM_RIGHT);
+
+        btnAbout.setOnAction(event -> showCredits());
+
+        btnFullscreen.setOnAction(event -> stage.setFullScreen(!stage.isFullScreen()));
+
+        btnSettings.setOnAction(event -> {
+            SettingsWindow scanWindow = new SettingsWindow(scanFacade);
+            Stage scanWindowStage = new Stage();
+            Scene scanWindowScene = new Scene(scanWindow);
+            scanWindowStage.setScene(scanWindowScene);
+
+            // Set the stage ref in our settings window so that we
+            // can show on-screen pop-ups for adding scans
+            scanWindow.setStage(scanWindowStage);
+
+            scanWindowStage.show();
+
+        });
+
+        btnQuit.setOnAction(event -> stage.close());
+
+        return buttons;
+    }
+
+    private VBox generateTop() {
         Text txtTitle = new Text("EDUS2J Simulator");
         txtTitle.setFont(Font.font("Calibri", FontWeight.BOLD,
                 FontPosture.ITALIC, 36.0));
@@ -154,98 +211,35 @@ public class EDUS2View extends Application
 
         top.getChildren().add(topThird);
         AnchorPane.setRightAnchor(topThird, 5.0);
-        main.setTop(top);
         BorderPane.setAlignment(topThird, Pos.TOP_RIGHT);
+        return topThird;
+    }
 
-        // Setting up bottom right control buttons
-        Button btnAbout = new Button("About");
-        btnAbout.setFont(BUTTON_FONT);
-        Button btnFullscreen = new Button("Toggle Fullscreen");
-        btnFullscreen.setFont(BUTTON_FONT);
-        Button btnSettings = new Button("Settings");
-        btnSettings.setFont(BUTTON_FONT);
-        Button btnQuit = new Button("Quit");
-        btnQuit.setFont(BUTTON_FONT);
+    private void showCredits() {
+        BorderPane credits = new BorderPane();
+        Text header = new Text("EDUS2J Credits");
+        header.setFont(new Font(32.0));
+        credits.setTop(header);
+        header.setFont(Font.font("Calibri", FontWeight.BOLD,
+                FontPosture.ITALIC, 36.0));
+        BorderPane.setAlignment(header, Pos.TOP_CENTER);
+        Text details = new Text(
+                "Credits for this project go to: \n"
+                        + "Java Porting: Cameron Auser\n"
+                        + "Original Design: Paul Kulyk, Paul Olsynski\n"
+                        + "EDUS2 is an emergency department ultrasound simulator, "
+                        + "and EDUS2J is a port of this original software to Java.");
+        details.setFont(new Font("Calibri", 18.0));
+        credits.setCenter(details);
+        BorderPane.setAlignment(credits, Pos.CENTER);
 
-        HBox buttons = new HBox();
-        buttons.getChildren().addAll(btnAbout, btnFullscreen, btnSettings,
-                btnQuit);
-        buttons.setAlignment(Pos.BOTTOM_RIGHT);
+        Scene creditScene = new Scene(credits);
 
-        main.setBottom(buttons);
-
-        Rectangle placeholder = new Rectangle(640, 480);
-        placeholder.setFill(Color.TRANSPARENT);
-        main.setCenter(placeholder);
-        Scene scene = new Scene(main);
-        stage.setScene(scene);
-        stage.getIcons().add(getThumbnailImage());
-        stage.setTitle("EDUS2J");
-        stage.show();
-
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                processScanRequest();
-            } else {
-                currentScan += event.getText();
-            }
-        });
-
-        // When the about button is clicked, a little popup will show on-screen
-        // with credits for the program
-        btnAbout.setOnAction(event -> {
-            BorderPane credits = new BorderPane();
-            Text header = new Text("EDUS2J Credits");
-            header.setFont(new Font(32.0));
-            credits.setTop(header);
-            header.setFont(Font.font("Calibri", FontWeight.BOLD,
-                    FontPosture.ITALIC, 36.0));
-            BorderPane.setAlignment(header, Pos.TOP_CENTER);
-            Text details = new Text(
-                    "Credits for this project go to: \n"
-                            + "Java Porting: Cameron Auser\n"
-                            + "Original Design: Paul Kulyk, Paul Olsynski\n"
-                            + "EDUS2 is an emergency department ultrasound simulator, "
-                            + "and EDUS2J is a port of this original software to Java.");
-            details.setFont(new Font("Calibri", 18.0));
-            credits.setCenter(details);
-            BorderPane.setAlignment(credits, Pos.CENTER);
-
-            Scene scene1 = new Scene(credits);
-
-            Stage test = new Stage();
-            test.getIcons().add(getThumbnailImage());
-            test.setScene(scene1);
-            test.setTitle("EDUS2J Credits");
-            test.show();
-        });
-
-        // The fullscreen button will just toggle fullscreen status of the
-        // program
-        btnFullscreen.setOnAction(event -> {
-            // Just set the program to be the opposite of what it's current
-            // fullscreen status is
-            stage.setFullScreen(!stage.isFullScreen());
-        });
-
-        // When the settings button is pressed, we'll create a SettingsWindow
-        // object and show it on-screen
-        btnSettings.setOnAction(event -> {
-            SettingsWindow scanWindow = new SettingsWindow(scanFacade);
-            Stage temp = new Stage();
-            Scene tempScene = new Scene(scanWindow);
-            temp.setScene(tempScene);
-
-            // Set the stage ref in our settings window so that we
-            // can show on-screen pop-ups for adding scans
-            scanWindow.setStage(temp);
-
-            temp.show();
-
-        });
-
-        // We'll close the program when the quit button is clicked
-        btnQuit.setOnAction(event -> stage.close());
+        Stage creditStage = new Stage();
+        creditStage.getIcons().add(getThumbnailImage());
+        creditStage.setScene(creditScene);
+        creditStage.setTitle("EDUS2J Credits");
+        creditStage.show();
     }
 
     private void processScanRequest() {
