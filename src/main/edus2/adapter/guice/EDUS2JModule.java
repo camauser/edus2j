b833f0fff34dd486c9c6dcf00609983d432a2fe8
Cameron Auser <camauser@gmail.com>
2019-11-06 19:46:07 -0600

    Binding FileEDUS2Configuration, adding test constructor.

--- src/main/edus2/adapter/guice/EDUS2JModule.java
+++ src/main/edus2/adapter/guice/EDUS2JModule.java
@@ -2,9 +2,11 @@ package edus2.adapter.guice;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import edus2.adapter.repository.file.FileEDUS2Configuration;
 import edus2.adapter.repository.file.FileScanRepository;
 import edus2.application.EDUS2View;
 import edus2.application.ScanFacade;
+import edus2.domain.EDUS2Configuration;
 import edus2.domain.ScanRepository;
 
 public class EDUS2JModule extends AbstractModule {
@@ -12,6 +14,8 @@ public class EDUS2JModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(FileScanRepository.class).toInstance(new FileScanRepository(EDUS2View.EDUS2_SAVE_FILE_NAME));
+
+        bind(EDUS2Configuration.class).toInstance(new FileEDUS2Configuration(System.getenv("APPDATA") + "\\edus2j"));
 
         bind(ScanRepository.class).to(FileScanRepository.class).in(Singleton.class);
 
