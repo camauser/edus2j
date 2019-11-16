d2b06d7eeef9c2258e5ce23763e1aee64578246c
Cameron Auser <camauser@gmail.com>
2019-11-16 16:21:30 -0600

    Implementing scan file import/export functionality with JSON input/output.

---
+++ src/main/edus2/adapter/persistence/FileScanImportExportRepository.java
@@ -0,0 +1,52 @@
+package edus2.adapter.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.inject.Inject;
+import edus2.application.ScanFacade;
+import edus2.domain.Scan;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileScanImportExportRepository {
+    private ScanFacade scanFacade;
+    private Gson gson;
+
+    @Inject
+    public FileScanImportExportRepository(ScanFacade scanFacade) {
+        this.scanFacade = scanFacade;
+        this.gson = new GsonBuilder().create();
+    }
+
+    public void exportScansToFile(File outputFile) throws IOException {
+        List<ScanDto> scanDtos = scanFacade.getAllScans().stream().map(ScanDto::new).collect(Collectors.toList());
+        Files.write(outputFile.toPath(), gson.toJson(scanDtos).getBytes());
+    }
+
+    public void importScansFromFile(File inputFile) throws IOException {
+        byte[] fileContents = Files.readAllBytes(inputFile.toPath());
+        Type type = new TypeToken<List<ScanDto>>(){}.getType();
+        List<ScanDto> scanDtos = gson.fromJson(new String(fileContents), type);
+        List<Scan> scans = scanDtos.stream().map(ScanDto::toScan).collect(Collectors.toList());
+        scanFacade.addScans(scans);
+    }
+
+    private static class ScanDto {
+        private String id;
+        private String path;
+        ScanDto(Scan scan) {
+            this.id = scan.getId();
+            this.path = scan.getPath();
+        }
+
+        Scan toScan() {
+            return new Scan(this.id, this.path);
+        }
+    }
+}
---
+++ src/test/edus2/adapter/persistence/FileScanImportExportRepositoryTest.java
@@ -0,0 +1,77 @@
+package edus2.adapter.persistence;
+
+import edus2.adapter.repository.memory.InMemoryScanRepository;
+import edus2.application.ScanFacade;
+import edus2.domain.Scan;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static edus2.TestUtil.Lst;
+import static edus2.TestUtil.randomTempFile;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class FileScanImportExportRepositoryTest {
+
+    private ScanFacade scanFacade;
+    private FileScanImportExportRepository repository;
+
+    @Before
+    public void setup() {
+        scanFacade = new ScanFacade(new InMemoryScanRepository());
+        repository = new FileScanImportExportRepository(scanFacade);
+    }
+
+    @Test
+    public void importScans_shouldImportScansInFile() throws IOException {
+        // Arrange
+        String fileName = randomTempFile();
+        Files.write(Paths.get(fileName), "[{\"id\": \"myScan\", \"path\": \"/path/to/scan\"}, {\"id\": \"secondScan\", \"path\": \"/path/to/second/scan\"}]".getBytes());
+
+        // Act
+        repository.importScansFromFile(new File(fileName));
+
+        // Assert
+        List<Scan> scans = scanFacade.getAllScans();
+        assertEquals(2, scans.size());
+        assertTrue(scans.contains(new Scan("myScan", "/path/to/scan")));
+        assertTrue(scans.contains(new Scan("secondScan", "/path/to/second/scan")));
+    }
+
+    @Test
+    public void exportScans_shouldExportScansToFile() throws IOException {
+        // Arrange
+        String fileName = randomTempFile();
+        scanFacade.addScans(Lst(new Scan("scan", "/scan/path"), new Scan("anotherScan", "/second/path")));
+
+        // Act
+        repository.exportScansToFile(new File(fileName));
+
+        // Assert
+        String result = new String(Files.readAllBytes(Paths.get(fileName)));
+        assertEquals("[{\"id\":\"scan\",\"path\":\"/scan/path\"},{\"id\":\"anotherScan\",\"path\":\"/second/path\"}]", result);
+    }
+
+    @Test
+    public void exportImport_endToEnd() throws IOException {
+        // Arrange
+        String fileName = randomTempFile();
+        List<Scan> scans = Lst(new Scan("scan", "/scan/path"), new Scan("anotherScan", "/second/path"));
+        scanFacade.addScans(scans);
+        repository.exportScansToFile(new File(fileName));
+        scanFacade.removeAllScans();
+
+        // Act
+        repository.importScansFromFile(new File(fileName));
+
+        // Assert
+        List<Scan> actual = scanFacade.getAllScans();
+        assertEquals(scans, actual);
+    }
+}
