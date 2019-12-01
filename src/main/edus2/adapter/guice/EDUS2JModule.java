e6dc6ad02673f6dae4fd449fedbceb249cbd0a01
Cameron Auser <camauser@gmail.com>
2019-12-01 11:47:11 -0600

    Injecting FileMannequinRepository.

--- src/main/edus2/adapter/guice/EDUS2JModule.java
+++ src/main/edus2/adapter/guice/EDUS2JModule.java
@@ -3,9 +3,11 @@ package edus2.adapter.guice;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import edus2.adapter.guice.provider.FileEDUS2ConfigurationProvider;
+import edus2.adapter.repository.file.FileMannequinRepository;
 import edus2.adapter.repository.file.FileScanRepository;
 import edus2.application.ScanFacade;
 import edus2.domain.EDUS2Configuration;
+import edus2.domain.MannequinRepository;
 import edus2.domain.ScanRepository;
 
 public class EDUS2JModule extends AbstractModule {
@@ -17,6 +19,8 @@ public class EDUS2JModule extends AbstractModule {
         bind(EDUS2Configuration.class).toProvider(FileEDUS2ConfigurationProvider.class).in(Singleton.class);
 
         bind(ScanRepository.class).to(FileScanRepository.class).in(Singleton.class);
+
+        bind(MannequinRepository.class).to(FileMannequinRepository.class).in(Singleton.class);
 
         bind(ScanFacade.class).in(Singleton.class);
     }
