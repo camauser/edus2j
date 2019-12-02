e69928feb5d18e4ba994c2791858a0bc6742a0ff
Cameron Auser <camauser@gmail.com>
2019-12-01 19:16:33 -0600

    Adding UI to update mannequin name and tags.

--- src/main/edus2/adapter/ui/MannequinCreationWindow.java
+++ src/main/edus2/adapter/ui/MannequinCreationWindow.java
@@ -1,7 +1,12 @@
 package edus2.adapter.ui;
 
 import edus2.application.MannequinFacade;
+import edus2.domain.InvalidMannequinNameException;
+import edus2.domain.Mannequin;
+import edus2.domain.MannequinScanEnum;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,26 +15,27 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Optional;
 
 public class MannequinCreationWindow extends HBox {
     private GridPane mannequinFieldEntry;
-    private final TextField mannequinName;
-    private final TextField cardiacSc;
-    private final TextField ivc;
-    private final TextField ruq;
-    private final TextField luq;
-    private final TextField abdominalAorta;
-    private final TextField pelvis;
+    private TextField mannequinName;
+    private TextField cardiacSc;
+    private TextField ivc;
+    private TextField ruq;
+    private TextField luq;
+    private TextField abdominalAorta;
+    private TextField pelvis;
+    private TextField rightLungScan;
+    private TextField leftLungScan;
+    private TextField cardiacPslPss;
+    private TextField cardiacA4c;
     private MannequinFacade mannequinFacade;
-    private final TextField rightLungScan;
-    private final TextField leftLungScan;
-    private final TextField cardiacPslPss;
-    private final TextField cardiacA4c;
     private int fieldsAdded = 0;
 
     public MannequinCreationWindow(MannequinFacade mannequinFacade) {
         super(20.0);
-//        this.setPrefColumns(2);
         this.mannequinFieldEntry = new GridPane();
         mannequinFieldEntry.setHgap(10.0);
         mannequinFieldEntry.setVgap(5.0);
@@ -37,6 +43,35 @@ public class MannequinCreationWindow extends HBox {
         File imageFile = new File("img/mannequin-scanpoints.png");
         Image scanPointImage = new Image("file:///" + imageFile.getAbsolutePath());
 
+        generateInputFields();
+        ImageView imageView = new ImageView(scanPointImage);
+        mannequinFieldEntry.setAlignment(Pos.CENTER);
+        this.getChildren().addAll(mannequinFieldEntry, imageView);
+    }
+
+    public void bindMannequin(String name) {
+        clearFields();
+        Optional<Mannequin> mannequinOptional = mannequinFacade.getMannequin(name);
+        if (!mannequinOptional.isPresent()) {
+            throw new InvalidMannequinNameException(String.format("Mannequin '%s' does not exist", name));
+        }
+
+        Mannequin mannequin = mannequinOptional.get();
+        mannequinName.setText(mannequin.getName());
+        mannequinName.setEditable(false);
+        rightLungScan.setText(mannequin.getTagMap().get(MannequinScanEnum.RIGHT_LUNG));
+        leftLungScan.setText(mannequin.getTagMap().get(MannequinScanEnum.LEFT_LUNG));
+        cardiacPslPss.setText(mannequin.getTagMap().get(MannequinScanEnum.CARDIAC_PSL_PSS));
+        cardiacA4c.setText(mannequin.getTagMap().get(MannequinScanEnum.CARDIAC_A4C));
+        cardiacSc.setText(mannequin.getTagMap().get(MannequinScanEnum.CARDIAC_SC));
+        ivc.setText(mannequin.getTagMap().get(MannequinScanEnum.IVC));
+        ruq.setText(mannequin.getTagMap().get(MannequinScanEnum.RUQ));
+        luq.setText(mannequin.getTagMap().get(MannequinScanEnum.LUQ));
+        abdominalAorta.setText(mannequin.getTagMap().get(MannequinScanEnum.ABDOMINAL_AORTA));
+        pelvis.setText(mannequin.getTagMap().get(MannequinScanEnum.PELVIS));
+    }
+
+    private void generateInputFields() {
         mannequinName = generateEntryBox("Mannequin Name");
         rightLungScan = generateEntryBox("(1) Right Lung Scan");
         leftLungScan = generateEntryBox("(2) Left Lung Scan");
@@ -48,9 +83,42 @@ public class MannequinCreationWindow extends HBox {
         luq = generateEntryBox("(8) LUQ");
         abdominalAorta = generateEntryBox("(9) Abdominal Aorta");
         pelvis = generateEntryBox("(10) Pelvis");
-        ImageView imageView = new ImageView(scanPointImage);
-        mannequinFieldEntry.setAlignment(Pos.CENTER);
-        this.getChildren().addAll(mannequinFieldEntry, imageView);
+        Button btnSaveMannequin = new Button("Save Mannequin");
+        btnSaveMannequin.setOnAction(action -> {
+            saveMannequin();
+        });
+        mannequinFieldEntry.add(btnSaveMannequin, 0, fieldsAdded);
+        fieldsAdded++;
+    }
+
+    private void saveMannequin() {
+        HashMap<MannequinScanEnum, String> tagMap = new HashMap<>();
+        tagMap.put(MannequinScanEnum.RIGHT_LUNG, rightLungScan.getText());
+        tagMap.put(MannequinScanEnum.LEFT_LUNG, leftLungScan.getText());
+        tagMap.put(MannequinScanEnum.CARDIAC_PSL_PSS, cardiacPslPss.getText());
+        tagMap.put(MannequinScanEnum.CARDIAC_A4C, cardiacA4c.getText());
+        tagMap.put(MannequinScanEnum.CARDIAC_SC, cardiacSc.getText());
+        tagMap.put(MannequinScanEnum.IVC, ivc.getText());
+        tagMap.put(MannequinScanEnum.RUQ, ruq.getText());
+        tagMap.put(MannequinScanEnum.LUQ, luq.getText());
+        tagMap.put(MannequinScanEnum.ABDOMINAL_AORTA, abdominalAorta.getText());
+        tagMap.put(MannequinScanEnum.PELVIS, pelvis.getText());
+        try {
+            Mannequin mannequin = new Mannequin(tagMap, mannequinName.getText());
+            mannequinFacade.save(mannequin);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, String.format("Successfully saved mannequin '%s'", mannequin.getName()));
+            alert.showAndWait();
+            clearFields();
+        } catch (RuntimeException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, String.format("Error saving mannequin: %s", e.getMessage()));
+            alert.showAndWait();
+        }
+    }
+
+    private void clearFields() {
+        fieldsAdded = 0;
+        mannequinFieldEntry.getChildren().clear();
+        generateInputFields();
     }
 
     private TextField generateEntryBox(String prompt) {
--- src/main/edus2/adapter/ui/MannequinSettingsWindow.java
+++ src/main/edus2/adapter/ui/MannequinSettingsWindow.java
@@ -4,10 +4,14 @@ import edus2.application.MannequinFacade;
 import edus2.domain.Mannequin;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class MannequinSettingsWindow extends VBox{
     private MannequinsWindow mannequinDisplay;
@@ -19,6 +23,8 @@ public class MannequinSettingsWindow extends VBox{
         HBox scanSettingButtonsBox = new HBox();
 
         Button btnAdd = new Button("Add");
+        Button btnUpdate = new Button("Update Scan Tags");
+        Button btnChangeName = new Button("Update Name");
         Button btnDelete = new Button("Delete");
 
         // When add is clicked, run through the process of adding a new scan
@@ -27,19 +33,57 @@ public class MannequinSettingsWindow extends VBox{
             Stage stage = new Stage();
             Scene scene = new Scene(mannequinSettingsWindow);
             stage.setScene(scene);
-            stage.setWidth(mannequinSettingsWindow.getWidth());
-            stage.show();
+            stage.showAndWait();
+            mannequinDisplay.refreshTableItems();
+        });
+
+        btnUpdate.setOnAction(event -> {
+            Mannequin selected = mannequinDisplay.getSelectedItem();
+            if (selected == null) {
+                return;
+            }
+            MannequinCreationWindow mannequinSettingsWindow = new MannequinCreationWindow(mannequinFacade);
+            mannequinSettingsWindow.bindMannequin(selected.getName());
+            Stage stage = new Stage();
+            Scene scene = new Scene(mannequinSettingsWindow);
+            stage.setScene(scene);
+            stage.showAndWait();
+            mannequinDisplay.refreshTableItems();
+        });
+
+        btnChangeName.setOnAction(event -> {
+            Mannequin selected = mannequinDisplay.getSelectedItem();
+            if (selected == null) {
+                return;
+            }
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setContentText("Please enter the new mannequin name:");
+            Optional<String> newNameOptional = dialog.showAndWait();
+            if (newNameOptional.isPresent()) {
+                try {
+                    mannequinFacade.rename(selected.getName(), newNameOptional.get());
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION, String.format("Mannequin '%s' has been renamed to '%s'", selected.getName(), newNameOptional.get()));
+                    alert.showAndWait();
+                } catch (Exception e) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, String.format("Error changing name: %s", e.getMessage()));
+                    alert.showAndWait();
+                }
+                mannequinDisplay.refreshTableItems();
+            }
         });
 
         // Delete the selected scan when the delete button is clicked.
         btnDelete.setOnAction(event -> {
             Mannequin selected = mannequinDisplay.getSelectedItem();
+            if (selected == null) {
+                return;
+            }
             mannequinFacade.remove(selected);
             mannequinDisplay.refreshTableItems();
         });
 
         scanSettingButtonsBox.setAlignment(Pos.CENTER);
-        scanSettingButtonsBox.getChildren().addAll(btnAdd, btnDelete);
+        scanSettingButtonsBox.getChildren().addAll(btnAdd, btnUpdate, btnChangeName, btnDelete);
 
         this.getChildren().addAll(mannequinDisplay, scanSettingButtonsBox);
 
