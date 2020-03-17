dbea85dde568afeff19e18db1d25cfc5f8d44279
Cameron Auser <camauser@gmail.com>
2020-03-17 16:40:48 -0600

    Adding tests around SystemIdentifier.

--- src/main/java/edus2/domain/SystemIdentifier.java
+++ src/main/java/edus2/domain/SystemIdentifier.java
@@ -1,5 +1,8 @@
 package edus2.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.util.UUID;
 
 public class SystemIdentifier {
@@ -19,5 +22,25 @@ public class SystemIdentifier {
 
     public static SystemIdentifier ofRandom() {
         return new SystemIdentifier(UUID.randomUUID());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SystemIdentifier that = (SystemIdentifier) o;
+
+        return new EqualsBuilder()
+                .append(systemIdentifier, that.systemIdentifier)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(systemIdentifier)
+                .toHashCode();
     }
 }
